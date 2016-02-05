7565a24967509132e160782c9bcefbee7803f289
robertsturner52 <robertsturner52@gmail.com>
2016-02-05 02:07:18 -0800

    initial commit

---
+++ scenario/app/controllers/woods_controller.rb
@@ -0,0 +1,22 @@
+class WoodsController < ApplicationController
+  def stream
+  end
+
+  def mountain
+  end
+
+  def swim
+  end
+
+  def fish
+  end
+
+  def hike
+  end
+
+  def climb
+  end
+
+  def forest
+  end
+end
